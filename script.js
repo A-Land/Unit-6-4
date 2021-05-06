42aebb6b4deca5c9abef4c03fbc0d7bb5845ef42
Adam Landriault <>
2021-05-06 15:26:46 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myBase1 = parseInt(prompt('Please enter base1 of trapezoid:'))
 const myBase2 = parseInt(prompt('Please enter base2 of trapezoid:'))
 const tHeight = parseInt(prompt('Please enter height of trapezoid:'))
-const areaOfTrapezoid =(myBase1 + myBase2 ) * tHeight / 2
-console.log("Area of trapezoid is: " + areaOfTrapezoid)
+const areaOfTrapezoid = (myBase1 + myBase2 )* tHeight / 2
+console.log('Area of trapezoid is:' + areaOfTrapezoid)
