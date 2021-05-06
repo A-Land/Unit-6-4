0baa24d7930293d3837e170bf07fc443d9896bfa
AdamLandriault <>
2021-05-06 15:08:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+ var myBase1 = parseInt(prompt("Please enter base1 of trapezoid:"));
+var myBase2 = parseInt(prompt("Please enter base2 of trapezoid:"));
+var tHeight = parseInt(prompt("Please enter height of trapezoid:"));
+var areaOfTrapezoid = (myBase1 + myBase2 ) * tHeight / 2;
+console.log("Area of trapezoid is: " + areaOfTrapezoid);
